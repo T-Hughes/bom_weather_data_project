c4895b6c557332f137562bbaaf6e82cdc7341dd2
Hughes <hug138@csiro.au>
2020-03-17 14:48:53 +1100

    finish q1

--- scripts_data_cleaning.R
+++ scripts_data_cleaning.R
@@ -2,6 +2,9 @@
 
 library(tidyverse)
 
+# Question 1:For each station, how many days have a minimum temperature,
+# a maximum temperature and a rainfall measurement recorded?
+
 #open file
 
 bom_data <- read_csv("raw_data/BOM_data.csv")
@@ -18,4 +21,22 @@ temp_min_max<- separate(bom_data, Temp_min_max, into = c("min", "max"), sep = "/
 #filter out all rows that have min, max and rainfall data
 
 cutdown_min_max_rainfall <- filter(temp_min_max, min !="-", max!="-", Rainfall !="-")
+
+#group per station
+
+perstation_cutdown_min_max_rainfall <- group_by(cutdown_min_max_rainfall, Station_number)
+
+#summarise per station number that have min, max and rainfall data
+#Finish Question 1:
+
+Bom_data_number <- summarise(perstation_cutdown_min_max_rainfall,
+                             days_recorded_min_max_rainfall=n())
+
+
+   
+
+
+# Question 2: Which month saw the lowest average daily temperature difference?
+
+
 
